1122e220476f31b01810289b4e715ff055b0400e
arabicmorph <camile.demyanenko00@mail.ru>
2025-05-19 23:17:28 +0000

    Настройка сайта на PythonAnywhere

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask, json, request
+import logging
+import git
+
+logging.basicConfig(level=logging.DEBUG)
+
+app = Flask(__name__)
+
+@app.route('/update_server', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('mysite')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
+
+@app.route('/')
+def hello_world():
+    return 'Я ненавижу HTML'
